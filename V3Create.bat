f95f8788943ad62dbfd229d58194a1e5bba96baf
Vic4mz9 <Vic4mz9@WDAUD70209LW.internal.vic.gov.au>
2019-11-22 16:52:31 +1100

    Removed bin variable from bat files to make it more portable

--- V3Create.bat
+++ V3Create.bat
@@ -1,7 +1,5 @@
 @echo off
 set code="C:\Users\Andrew\Documents\Work\VERSCode\neoVEO"
-set bin="C:\Program Files\Java\jdk1.8.0_162\bin"
 rem set code="J:\PROV\TECHNOLOGY MANAGEMENT\Application Development\VERS\VERSCode\neoVEO"
-rem set bin="C:\Program Files\Java\jdk1.8.0_144\bin"
 set versclasspath=%code%/dist/*
-%bin%\java -classpath %versclasspath% VEOCreate.CreateVEOs -v -t %code%/neoVEOTemplates %*
+java -classpath %versclasspath% VEOCreate.CreateVEOs -v -t %code%/neoVEOTemplates %*
--- V3Resign.bat
+++ V3Resign.bat
@@ -1,7 +1,5 @@
 @echo off
 rem set code="C:\Users\Andrew\Documents\Work\VERS-2015\VPA"
-rem set bin="C:\Program Files\Java\jdk1.8.0_162\bin"
 set code="J:\PROV\TECHNOLOGY MANAGEMENT\Application Development\VERS\VERS-2015\neoVEO"
-set bin="C:\Program Files\Java\jdk1.8.0_144\bin"
 set versclasspath=%code%/dist/*
-%bin%\java -classpath %versclasspath% VPA.DAIngest %*
+java -classpath %versclasspath% VPA.DAIngest %*
